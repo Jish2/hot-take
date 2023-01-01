e965aeb067a8224275f889b4013d24f08c4406e8
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-01 13:16:52 -0600

    prettier reformatting

--- frontend/components/hotTakeCard.js
+++ frontend/components/hotTakeCard.js
@@ -1,42 +1,51 @@
-import React from 'react'
-import {Stack, Heading, Divider, ButtonGroup, Button, Image, Text, Card, CardHeader, CardBody, CardFooter, HStack } from '@chakra-ui/react'
+import React from "react";
+import {
+	Stack,
+	Heading,
+	Divider,
+	ButtonGroup,
+	Button,
+	Image,
+	Text,
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	HStack,
+} from "@chakra-ui/react";
 
-
-export default function HotTakeCard( { title }) {
-  return (
-    <div style={{scrollSnapAlign:"center", height:'100vh', display:'flex',justifyContent:'center',alignItems:'center'}}>
-
-        <Card >
-        <CardBody>
-            
-            <Stack mt='6' spacing='3'>
-
-            <Heading size='lg'>{title}</Heading>
-            
-            
-            </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-            <ButtonGroup spacing='2'>
-            <Button variant='solid' colorScheme='blue'>
-                <HStack>
-                    
-                    <p>Upvote</p>
-                    <p>1.9k</p>
-                    
-                </HStack>
-                
-            </Button>
-            <Button variant='ghost' colorScheme='blue'>
-                Downvote
-            </Button>
-            </ButtonGroup>
-        </CardFooter>
-        </Card>
-
-        
-    </div>
-            
-  )
+export default function HotTakeCard({ title }) {
+	return (
+		<div
+			style={{
+				scrollSnapAlign: "center",
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Card>
+				<CardBody>
+					<Stack mt="6" spacing="3">
+						<Heading size="lg">{title}</Heading>
+					</Stack>
+				</CardBody>
+				<Divider />
+				<CardFooter>
+					<ButtonGroup spacing="2">
+						<Button variant="solid" colorScheme="blue">
+							<HStack>
+								<p>Upvote</p>
+								<p>1.9k</p>
+							</HStack>
+						</Button>
+						<Button variant="ghost" colorScheme="blue">
+							Downvote
+						</Button>
+					</ButtonGroup>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,69 +1,96 @@
-'use client'
-import { Inter } from '@next/font/google'
-import { VStack, Text,Center, useDisclosure, Button, Container, HStack,Box,Tag,Image} from '@chakra-ui/react'
-import HotTakeCard from '../components/hotTakeCard'
-import React, {useState, useRef, useEffect} from 'react';
-import { ChakraProvider } from '@chakra-ui/react'
-import UploadHotTake from '../components/uploadHT';
-import useScrollSnap from 'react-use-scroll-snap';
+"use client";
+import { Inter } from "@next/font/google";
+import {
+	VStack,
+	Text,
+	Center,
+	useDisclosure,
+	Button,
+	Container,
+	HStack,
+	Box,
+	Tag,
+	Image,
+} from "@chakra-ui/react";
+import HotTakeCard from "../components/hotTakeCard";
+import React, { useState, useRef, useEffect } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import UploadHotTake from "../components/uploadHT";
+import useScrollSnap from "react-use-scroll-snap";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
-
-
+const inter = Inter({ subsets: ["latin"] });
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const res = await fetch('http://localhost:3001/posts')
-  const postsFromDB = await res.json()
-  //console.log(postsFromDB)
+	// Call an external API endpoint to get posts.
+	// You can use any data fetching library
+	const res = await fetch("http://localhost:3001/posts");
+	const postsFromDB = await res.json();
+	//console.log(postsFromDB)
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      postsFromDB,
-    },
-  }
+	// By returning { props: { posts } }, the Blog component
+	// will receive `posts` as a prop at build time
+	return {
+		props: {
+			postsFromDB,
+		},
+	};
 }
 
-export default function Home({postsFromDB}) {
-  // const scrollRef = useRef(null);
-  // useScrollSnap({ ref: scrollRef, duration: 300, delay: 0 });
+export default function Home({ postsFromDB }) {
+	// const scrollRef = useRef(null);
+	// useScrollSnap({ ref: scrollRef, duration: 300, delay: 0 });
 
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	//const posts = await getPosts();
+	//on load, check if the user has a uuid stored
+	useEffect(() => {
+		console.log(postsFromDB);
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  //const posts = await getPosts();
-  //on load, check if the user has a uuid stored
-  useEffect(()=>{
-    console.log(postsFromDB)
-    
-    
-    if (localStorage.getItem('uuid') == null){
-      console.log("UUID has not been found, creating UUID")
-      localStorage.setItem('uuid', uuidv4())
-    }
-    
+		if (localStorage.getItem("uuid") == null) {
+			console.log("UUID has not been found, creating UUID");
+			localStorage.setItem("uuid", uuidv4());
+		}
+	}, []);
 
-  }, [])
+	const [posts, setPosts] = useState(postsFromDB);
+	//console.log(posts)
 
-  const [posts, setPosts] = useState(postsFromDB)
-  //console.log(posts)
-
-
-  return (
-    <>
-       <UploadHotTake isOpen={isOpen} onClose={onClose}/>
-      <Button onClick={onOpen} colorScheme='twitter' size='lg' style={{aspectRatio:"1/1", borderRadius:"100%", position:"fixed", right:'10vw', bottom:'10vh'}} >+</Button>
-      <Container m={0} w="100%" style={{marginLeft:"auto", marginRight:"auto", height:"100vh", scrollSnapType:"y mandatory",overflowY:"scroll"}} w="full" >
-        {posts.map((post,i)=><div style={{scrollSnapAlign:"center"}}><HotTakeCard key={i} title={post.title} /></div>)}
-      </Container> 
-      
-        
-    </>
-  )
+	return (
+		<>
+			<UploadHotTake isOpen={isOpen} onClose={onClose} />
+			<Button
+				onClick={onOpen}
+				colorScheme="twitter"
+				size="lg"
+				style={{
+					aspectRatio: "1/1",
+					borderRadius: "100%",
+					position: "fixed",
+					right: "10vw",
+					bottom: "10vh",
+				}}
+			>
+				+
+			</Button>
+			<Container
+				m={0}
+				w="100%"
+				style={{
+					marginLeft: "auto",
+					marginRight: "auto",
+					height: "100vh",
+					scrollSnapType: "y mandatory",
+					overflowY: "scroll",
+				}}
+			>
+				{posts.map((post, i) => (
+					<div style={{ scrollSnapAlign: "center" }}>
+						<HotTakeCard key={i} title={post.title} />
+					</div>
+				))}
+			</Container>
+		</>
+	);
 }
