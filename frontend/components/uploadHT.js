13c43a6a5961a30894d870a7595bfc045e02b8f2
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-01 19:13:31 -0600

    Made modal not extend across entire screen

--- frontend/components/uploadHT.js
+++ frontend/components/uploadHT.js
@@ -1,21 +1,11 @@
 import { React, useState, useRef } from "react";
-import {
-	Button,
-	Modal,
-	ModalOverlay,
-	ModalHeader,
-	ModalCloseButton,
-	ModalBody,
-	Textarea,
-	ModalFooter,
-	ModalContent,
-} from "@chakra-ui/react";
+// prettier-ignore
+import { Button, Textarea, Modal, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, ModalContent } from "@chakra-ui/react";
 import axios, { isCancel, AxiosError } from "axios";
 
 export default function UploadHotTake({ isOpen, onClose }) {
 	const handlePostSubmit = (e) => {
 		e.preventDefault();
-		//console.log("asshole")
 		axios
 			.post("http://localhost:3001/post", {
 				title: input.current.value,
@@ -26,8 +16,6 @@ export default function UploadHotTake({ isOpen, onClose }) {
 			.catch(function (error) {
 				console.log(error);
 			});
-
-		// POST request
 	};
 	const input = useRef(null);
 
@@ -35,7 +23,7 @@ export default function UploadHotTake({ isOpen, onClose }) {
 		<>
 			<Modal isOpen={isOpen} onClose={onClose} motionPreset="scale" isCentered>
 				<ModalOverlay />
-				<ModalContent>
+				<ModalContent style={{ width: "90%" }}>
 					<ModalHeader>Share a hot take of up to 140 words!</ModalHeader>
 					<ModalCloseButton />
 					<form onSubmit={handlePostSubmit}>
