965011d728034856b20cbbe60d3edbe01657d434
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-04 20:34:20 -0600

    enclosing div (had error)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,8 +2,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<div>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</div>
 	);
 }
