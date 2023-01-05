e00a4fd6b1ff01710e7b22d5a3ed0b4ba4979d02
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-05 04:17:53 -0600

    sort by interactions

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,8 +53,8 @@ const remove = (value, array) => {
 
 // fetch posts
 app.get("/posts", fetchPostLimiter, async (req, res) => {
-	// sorts collection so newest posts are first
-	const postsLists = await Post.find().sort({ date: -1 });
+	// sorts collection so most interacted with posts are first
+	const postsLists = await Post.find().sort({ interactions: -1 });
 	res.send(postsLists);
 });
 
