e0997bd3a554136045c81237ce33355fa45c6ed4
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-05 16:23:57 -0600

    some more refactoring

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 import { AiFillFire } from "react-icons/ai";
 import Link from "next/link";
 
-const LOGO_TEXT = "Hot Take";
+const LOGO_TEXT = "HotTake";
 const NAV_ITEMS = [
 	{
 		label: "About",
@@ -109,10 +109,10 @@ export function Navbar() {
 							fontSize={"sm"}
 							fontWeight={600}
 							color={"white"}
-							bg={"pink.400"}
+							bg={"teal.400"}
 							href={"#"}
 							_hover={{
-								bg: "pink.300",
+								bg: "teal.300",
 							}}
 						>
 							Sign Up
@@ -185,13 +185,13 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
 			display={"block"}
 			p={2}
 			rounded={"md"}
-			_hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+			_hover={{ bg: useColorModeValue("teal.50", "gray.900") }}
 		>
 			<Stack direction={"row"} align={"center"}>
 				<Box>
 					<Text
 						transition={"all .3s ease"}
-						_groupHover={{ color: "pink.400" }}
+						_groupHover={{ color: "teal.400" }}
 						fontWeight={500}
 					>
 						{label}
@@ -207,7 +207,7 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
 					align={"center"}
 					flex={1}
 				>
-					<Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+					<Icon color={"teal.400"} w={5} h={5} as={ChevronRightIcon} />
 				</Flex>
 			</Stack>
 		</Link>
--- frontend/pages/about/index.jsx
+++ frontend/pages/about/index.jsx
@@ -1,62 +1,69 @@
 import React from "react";
-import { Container, Heading, Image, Text,Button, HStack, Icon } from "@chakra-ui/react";
-import { AiFillFire } from "react-icons/ai";
+import { Container, Heading, Image, Text, Button } from "@chakra-ui/react";
 
-import WithSubnavigation from "../../components/ChakraNavbar";
+import { Navbar } from "../../components/Navbar";
 export default function About() {
-  return (
-    <>
-      <WithSubnavigation />
-      <div style={{ height: "10vh" }}></div>
-      <Container>
-        <Heading mt={3} style={{ textAlign: "center" }}>
-          About HotTake
-        </Heading>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            marginTop: "5vh",
-          }}
-        >
-          <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-            <Image
+	return (
+		<>
+			<Navbar />
+			<div style={{ height: "60px" }}></div>
+			<Container>
+				<Heading mt={3} style={{ textAlign: "center" }}>
+					About HotTake
+				</Heading>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "space-around",
+						marginTop: "5vh",
+					}}
+				>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
+					>
+						<Image
+							borderRadius="full"
+							boxSize={{ base: "125px", sm: "150px" }}
+							src="https://i.postimg.cc/52zDJrby/hottakepfp-1.png"
+							alt="Dan Abramov"
+						/>
+						<Button mt={3} colorScheme="teal" size={{ base: "sm", sm: "md" }}>
+							CEO @ibrahimthespy
+						</Button>
+					</div>
 
-              borderRadius="full"
-              boxSize={{base:"125px",sm:"150px"}}
-              src="https://i.postimg.cc/52zDJrby/hottakepfp-1.png"
-              alt="Dan Abramov"
-            />
-            <Button mt={3} colorScheme="teal" size={{base:"sm", sm:"md"}}>
-              CEO @ibrahimthespy
-            </Button>
-
-          </div>
-
-          <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-            <Image
-              borderRadius="full"
-              boxSize={{base:"125px",sm:"150px"}}
-
-              src="https://i.postimg.cc/477KCk0k/josh-1.png"
-              alt="Dan Abramov"
-            />
-            <Button mt={3} colorScheme="teal" size={{base:"sm", sm:"md"}}>
-              CTO @jgoon3
-            </Button>
-
-          </div>
-          
-        </div>
-        <Text mt={10} style={{textAlign:"center"}}>
-          HotTake was founded in December of 2022, by founders Ibrahim Shah, and Joshua Goon.
-          Ibrahim Shah attends the University of California, Irvine, while Joshua Goon attends
-          the University of Illinois, Urbana Champaign, where they both study computer science.
-          HotTake was created as a platform where users can create and rate other people's 
-          hot takes within their college, connecting the community and providing comedic relief.
-        </Text>
-        
-      </Container>
-    </>
-  );
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
+					>
+						<Image
+							borderRadius="full"
+							boxSize={{ base: "125px", sm: "150px" }}
+							src="https://i.postimg.cc/477KCk0k/josh-1.png"
+							alt="Dan Abramov"
+						/>
+						<Button mt={3} colorScheme="teal" size={{ base: "sm", sm: "md" }}>
+							CTO @jgoon3
+						</Button>
+					</div>
+				</div>
+				<Text mt={10} style={{ textAlign: "center" }}>
+					HotTake was founded in December of 2022, by founders Ibrahim Shah, and
+					Joshua Goon. Ibrahim Shah attends the University of California,
+					Irvine, while Joshua Goon attends the University of Illinois, Urbana
+					Champaign, where they both study computer science. HotTake was created
+					as a platform where users can create and rate other people's hot takes
+					within their college, connecting the community and providing comedic
+					relief.
+				</Text>
+			</Container>
+		</>
+	);
 }
