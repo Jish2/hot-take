3caefaaa7092236531afe7744dd5fcf892763e6e
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-01-24 13:00:00 -0600

    static pages fixed

--- frontend/pages/internship/index.jsx
+++ frontend/pages/internship/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Text, Link } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 import { Navbar } from "../../components/Navbar";
 import { Footer } from "../../components/Footer";
 import { TitleBlock, ItemBlock, Spacer } from "../../components/StaticComponent";
@@ -8,18 +8,26 @@ export default function Internships() {
 		<>
 			<Navbar />
 			<div style={{ height: "10vh" }}></div>
-
 			<TitleBlock
 				title="Intern at HotTake"
 				url="https://www.arts.uci.edu/sites/default/files/Visit-StudentCenter.jpg"
 			/>
-
 			<ItemBlock title="">Summarize Intern Opportunities</ItemBlock>
-			<ItemBlock title="Software Engineer">Some text.</ItemBlock>
-			<ItemBlock title="Marketing Intern">Some text.</ItemBlock>
-
+			<ItemBlock title="Software Engineer">
+				Looking for those passionate about Web Development. Email your resume to{" "}
+				<Link color="teal.500" href="mailto:careers@hottake.gg">
+					careers@hottake.gg
+				</Link>{" "}
+				to apply.
+			</ItemBlock>
+			<ItemBlock title="Marketing Intern">
+				Looking for those who can pursue growth at Hottake. Email your resume to{" "}
+				<Link color="teal.500" href="mailto:careers@hottake.gg">
+					careers@hottake.gg
+				</Link>{" "}
+				to apply.
+			</ItemBlock>
 			<Spacer size={100} />
-
 			<Footer />
 		</>
 	);
