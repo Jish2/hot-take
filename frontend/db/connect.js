935e5495e8eba1c303e941defc3bb5b3fbb6a387
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-06-18 18:39:14 -0500

    edited production

--- frontend/db/connect.js
+++ frontend/db/connect.js
@@ -6,42 +6,56 @@ if (!MONGODB_URI) {
 	throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === "LOCAL") {
+	/**
+	 * Global is used here to maintain a cached connection across hot reloads
+	 * in development. This prevents connections growing exponentially
+	 * during API Route usage.
+	 */
+	let cached = global.mongoose;
 
-if (!cached) {
-	cached = global.mongoose = { conn: null, promise: null };
-}
+	if (!cached) {
+		cached = global.mongoose = { conn: null, promise: null };
+	}
 
-async function connect() {
-	if (cached.conn) {
+	async function connect() {
+		if (cached.conn) {
+			return cached.conn;
+		}
+
+		if (!cached.promise) {
+			const opts = {
+				bufferCommands: false,
+			};
+
+			mongoose.set("strictQuery", false);
+
+			cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+				return mongoose;
+			});
+		}
+
+		try {
+			cached.conn = await cached.promise;
+		} catch (e) {
+			cached.promise = null;
+			throw e;
+		}
+
 		return cached.conn;
 	}
+} else {
+	// In production mode, it's best to not use a global variable.
 
-	if (!cached.promise) {
-		const opts = {
-			bufferCommands: false,
-		};
+	const opts = {
+		bufferCommands: false,
+	};
 
-		mongoose.set("strictQuery", false);
+	mongoose.set("strictQuery", false);
 
-		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-			return mongoose;
-		});
-	}
-
-	try {
-		cached.conn = await cached.promise;
-	} catch (e) {
-		cached.promise = null;
-		throw e;
-	}
-
-	return cached.conn;
+	cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+		return mongoose;
+	});
 }
 
 export default connect;
