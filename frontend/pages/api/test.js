11bcc13a3916934956389ec985db063bd69ca217
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-06-18 21:27:44 -0500

    try

--- frontend/pages/api/test.js
+++ frontend/pages/api/test.js
@@ -4,10 +4,12 @@ export default async function handler(req, res) {
 	const { method } = req;
 
 	// await connect();
+	mongoose.set("strictQuery", true);
+	await mongoose.connect(process.env.MONGO_URL);
 
 	switch (method) {
 		case "GET":
-			res.status(200).json({ success: true, data: "MOMs" });
+			res.status(200).json({ success: true, data: "MOMs!" });
 			break;
 		default:
 			res.status(400).json({ success: false });
