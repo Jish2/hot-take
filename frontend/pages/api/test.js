fb39fa74296f4e2d05e83247c7f3ec95a0f87b5c
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-06-18 22:58:41 -0500

    lord hel pme

--- frontend/pages/api/test.js
+++ frontend/pages/api/test.js
@@ -1,18 +1,30 @@
 // import connect from "../../db/connect";
 import mongoose from "mongoose";
 
+export const config = {
+	api: {
+		externalResolver: true,
+	},
+};
+
 export default async function handler(req, res) {
 	const { method } = req;
 
 	// await connect();
+	// mongoose.set("strictQuery", true);
+	// mongoose.connect(process.env.MONGO_URL).then((response) => {
+	// 	res.status(200).json({ success: true, data: "MOMs111231!" });
+	// });
+
 	mongoose.set("strictQuery", true);
-	mongoose.connect(process.env.MONGO_URL).then((response) => {
-		res.status(200).json({ success: true, data: "MOMs111231!" });
+	const connection = mongoose.connect(MONGODB_URI).then((mongoose) => {
+		return mongoose;
 	});
+	await connection;
 
 	switch (method) {
 		case "GET":
-			res.status(200).json({ success: true, data: "MOMs!" });
+			res.status(200).json({ success: true, data: "MOMs!1aa" });
 			break;
 		default:
 			res.status(400).json({ success: false });
