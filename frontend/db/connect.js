ed64f777861e88dcef12ba1726d1e0ffe8a049f6
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-06-18 20:45:30 -0500

    rety

--- frontend/db/connect.js
+++ frontend/db/connect.js
@@ -6,46 +6,73 @@ if (!MONGODB_URI) {
 	throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
+// async function connect() {
+// 	if (process.env.NEXT_PUBLIC_ENVIRONMENT === "LOCAL") {
+// 		/**
+// 		 * Global is used here to maintain a cached connection across hot reloads
+// 		 * in development. This prevents connections growing exponentially
+// 		 * during API Route usage.
+// 		 */
+// 		let cached = global.mongoose;
+
+// 		if (!cached) {
+// 			cached = global.mongoose = { conn: null, promise: null };
+// 		}
+
+// 		if (cached.conn) {
+// 			return cached.conn;
+// 		}
+
+// 		if (!cached.promise) {
+// 			mongoose.set("strictQuery", false);
+
+// 			cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+// 				return mongoose;
+// 			});
+// 		}
+
+// 		try {
+// 			cached.conn = await cached.promise;
+// 		} catch (e) {
+// 			cached.promise = null;
+// 			throw e;
+// 		}
+
+// 		return cached.conn;
+// 	} else {
+// 		mongoose.set("strictQuery", false);
+
+// 		mongoose.connect(MONGODB_URI).then((mongoose) => {
+// 			return mongoose;
+// 		});
+// 	}
+// }
+
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function connect() {
-	if (process.env.NEXT_PUBLIC_ENVIRONMENT === "LOCAL") {
-		/**
-		 * Global is used here to maintain a cached connection across hot reloads
-		 * in development. This prevents connections growing exponentially
-		 * during API Route usage.
-		 */
-		let cached = global.mongoose;
-
-		if (!cached) {
-			cached = global.mongoose = { conn: null, promise: null };
-		}
-
-		if (cached.conn) {
-			return cached.conn;
-		}
-
-		if (!cached.promise) {
-			mongoose.set("strictQuery", false);
-
-			cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-				return mongoose;
-			});
-		}
-
-		try {
-			cached.conn = await cached.promise;
-		} catch (e) {
-			cached.promise = null;
-			throw e;
-		}
-
+	if (cached.conn) {
 		return cached.conn;
-	} else {
-		mongoose.set("strictQuery", false);
+	}
 
-		mongoose.connect(MONGODB_URI).then((mongoose) => {
+	if (!cached.promise) {
+		cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
 			return mongoose;
 		});
 	}
+
+	try {
+		cached.conn = await cached.promise;
+	} catch (e) {
+		cached.promise = null;
+		throw e;
+	}
+
+	return cached.conn;
 }
 
 export default connect;
