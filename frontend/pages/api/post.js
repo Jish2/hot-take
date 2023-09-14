cca5f113bbe3c0515532d9737666fb9aec5534a4
Andrew Ho <andyminhtuanho@gmail.com>
2023-09-13 21:11:32 -0700

    Added post rankings

--- frontend/pages/api/post.js
+++ frontend/pages/api/post.js
@@ -1,71 +1,81 @@
-import mongoose from "mongoose";
-import connect from "../../db/connect";
-import Post from "../../db/models/Post";
+import mongoose from 'mongoose';
+import connect from '../../db/connect';
+import Post from '../../db/models/Post';
+
 
 export const config = {
-	api: {
-		externalResolver: true,
-	},
+  api: {
+    externalResolver: true,
+  },
 };
 
 export default async function handler(req, res) {
-	const { method } = req;
+  const { method } = req;
 
-	await connect();
+  await connect();
 
-	switch (method) {
-		case "GET":
-			try {
-				const postID = req.query.postID;
+  switch (method) {
+    case 'GET':
+      try {
+        const postID = req.query.postID;
 
-				// catch invalid postID
-				if (mongoose.Types.ObjectId.isValid(postID) === false) {
-					res.status(400).json({ message: "stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy" });
-					return;
-				}
+        // catch invalid postID
+        if (mongoose.Types.ObjectId.isValid(postID) === false) {
+          res
+            .status(400)
+            .json({
+              message:
+                'stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy',
+            });
+          return;
+        }
 
-				// // catch invalid uuid
-				// const validUUID = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-				// if (!validUUID.test(postID)) {
-				// 	res.status(400).send("stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy");
-				// 	return;
-				// }
+        // // catch invalid uuid
+        // const validUUID = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+        // if (!validUUID.test(postID)) {
+        // 	res.status(400).send("stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy");
+        // 	return;
+        // }
 
-				const results = await Post.findById(postID).limit(1);
-				res.status(200).json(results);
-			} catch (error) {
-				console.error(error);
-				res.status(400).json({ message: error });
-			}
-			break;
-		case "POST":
-			try {
-				if (req.body.title.length == 0) {
-					res.status(400).json({ message: "Post content is missing" });
-				} else if (req.body.title.length <= 5) {
-					res.status(400).json({ message: "Post must be longer than 5 characters" });
-				} else if (req.body.title.length > 140) {
-					res.status(400).json({ message: "Post must be less than 140 characters" });
-				} else {
-					const newPost = new Post({
-						title: req.body.title,
-						agree: [],
-						disagree: [],
-						votes: 0,
-						interactions: 0,
-						reports: [],
-						date: new Date(),
-					});
-					const createdPost = await newPost.save();
-					res.status(200).json(createdPost);
-				}
-			} catch (error) {
-				console.error(error);
-				res.status(400).json({ message: error });
-			}
-			break;
-		default:
-			res.status(400).json({ message: error });
-			break;
-	}
+        const results = await Post.findById(postID).limit(1);
+        res.status(200).json(results);
+      } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: error });
+      }
+      break;
+    case 'POST':
+      try {
+        if (req.body.title.length == 0) {
+          res.status(400).json({ message: 'Post content is missing' });
+        } else if (req.body.title.length <= 5) {
+          res
+            .status(400)
+            .json({ message: 'Post must be longer than 5 characters' });
+        } else if (req.body.title.length > 140) {
+          res
+            .status(400)
+            .json({ message: 'Post must be less than 140 characters' });
+        } else {
+          const newPost = new Post({
+            title: req.body.title,
+            agree: [],
+            disagree: [],
+            votes: 0,
+            interactions: 0,
+            reports: [],
+            date: new Date(),
+          });
+          const createdPost = await newPost.save();
+          res.status(200).json(createdPost);
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: error });
+      }
+      break;
+    default:
+      res.status(400).json({ message: error });
+      break;
+  }
 }
--- frontend/pages/api/posts.js
+++ frontend/pages/api/posts.js
@@ -1,73 +1,144 @@
-import connect from "../../db/connect";
-import Post from "../../db/models/Post";
+import connect from '../../db/connect';
+import Post from '../../db/models/Post';
 
 export const config = {
-	api: {
-		externalResolver: true,
-	},
+  api: {
+    externalResolver: true,
+  },
 };
 
 export default async function handler(req, res) {
-	const { method, query } = req;
+  const { method, query } = req;
 
-	const limit = query.limit || 10; // default to 20
-	const offset = query.offset || 0;
-	const sort = req.query.sort;
+  const limit = query.limit || 10; // default to 20
+  const offset = query.offset || 0;
+  const sort = req.query.sort;
 
-	await connect();
+  await connect();
 
-	let postsLists = {};
+  let postsLists = {};
 
-	switch (method) {
-		case "GET":
-			try {
-				// const pets = await Pet.find({}); /* find all the data in our database */
-				// res.status(200).json({ success: true, data: pets });
+  switch (method) {
+    case 'GET':
+      try {
+        // const pets = await Pet.find({}); /* find all the data in our database */
+        // res.status(200).json({ success: true, data: pets });
 
-				if (limit < 0 || offset < 0) {
-					res.status(400).json({ message: "stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy" });
-					return;
-				}
+        if (limit < 0 || offset < 0) {
+          res.status(400).json({
+            message:
+              'stop trying to fuck with us you cs pussy dick es8 wanna be nerd pussy',
+          });
+          return;
+        }
 
-				switch (sort) {
-					case "new":
-						postsLists = { date: -1 };
-						break;
-					case "popular":
-						postsLists = { interactions: -1 };
-						break;
-					case "old":
-						postsLists = { date: 1 };
-						break;
-					case "random":
-						const results = await Post.aggregate([{ $sample: { size: limit } }]);
-						res.status(200).json(results);
-						return;
-					case "disagreed":
-						postsLists = { votes: 1 };
-						break;
-					case "agreed":
-						postsLists = { votes: -1 };
-						break;
-					case "reported":
-						postsLists = { reports: -1 };
-						break;
-					default:
-						// fallback to sorting by new
-						postsLists = { date: -1 };
-				}
-				const results = await Post.find().sort(postsLists).skip(offset).limit(limit);
-				res.status(200).json(results);
-			} catch (error) {
-				console.error("error", error);
-				res.status(400).json({ message: error, case: "hm" });
-				// res.status(400).json({ success: false });
-			}
-			break;
-		default:
-			// res.status(400).json({ success: false });
-			res.status(400).json({ message: "wack method" });
+        switch (sort) {
+          case 'hot':
+            break;
+          case 'new':
+            postsLists = { date: -1 };
+            break;
+          case 'popular':
+            postsLists = { interactions: -1 };
+            break;
+          case 'old':
+            postsLists = { date: 1 };
+            break;
+          case 'random':
+            const results = await Post.aggregate([
+              { $sample: { size: limit } },
+            ]);
+            res.status(200).json(results);
+            return;
+          case 'disagreed':
+            postsLists = { votes: 1 };
+            break;
+          case 'agreed':
+            postsLists = { votes: -1 };
+            break;
+          case 'reported':
+            postsLists = { reports: -1 };
+            break;
+          default:
+            // fallback to sorting by new
+            postsLists = { date: -1 };
+        }
+        // const results = await Post.find()
+        //   .sort(postsLists)
+        //   .skip(offset)
+        //   .limit(limit);
 
-			break;
-	}
+        const std = 0.4;
+        const mean = 0.31;
+        const engagementBonusCoef = 0.1;
+
+        const engagementBonus = {
+          $multiply: [
+            engagementBonusCoef,
+            {
+              $ln: {
+                $add: ['$interactions', 1],
+              },
+            },
+          ],
+        };
+
+        const x = {
+          $divide: [
+            {
+              $add: ['$votes', 1],
+            },
+            { $add: ['$interactions', 2] },
+          ],
+        };
+
+        const results = await Post.aggregate([
+          { $match: {} },
+          // 1/s *sqrt(2pi) * e^(-1/2 * x-m/s)^2
+          {
+            $addFields: {
+              rating: {
+                $add: [
+                  {
+                    $multiply: [
+                      1 / (std * Math.sqrt(2 * Math.PI)),
+                      {
+                        $exp: {
+                          $multiply: [
+                            -0.5,
+                            {
+                              $pow: [
+                                { $divide: [{ $subtract: [x, mean] }, std] },
+                                2,
+                              ],
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                  engagementBonus,
+                ],
+              },
+            },
+          },
+          { $sort: { rating: -1 } },
+          { $skip: offset },
+          { $limit: parseInt(limit) },
+          // { $project: { rating: 0 } },
+        ]);
+
+        res.status(200).json(JSON(results));
+      } catch (error) {
+        console.error('error', error);
+        res.status(400).json({ message: error, case: 'hm' });
+        // res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      // res.status(400).json({ success: false });
+      res.status(400).json({ message: 'wack method' });
+
+      break;
+  }
 }
